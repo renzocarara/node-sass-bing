d3f27be339da53d744f3c2ffbf2ffa9237a632ea
Renzo Carara <renzo.carara@libero.it>
2020-01-18 22:49:02 +0100

    solo layout, niente interazinoi popup, menu, etc

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+var $ = require('jquery'); // dichiaro che il mio progetto ha una dipendenza al package jquery 3.4.1
+
+// funziona solo per dropdown
+// window.Popper = require('popper.js/dist/umd/popper.js');
+// require('bootstrap/js/src/dropdown');
+// require('bootstrap/js/src/modal');
+// require('bootstrap/js/src/popover');
+// require('bootstrap/js/src/tooltip');
+
+
+// funziona solo per dropdown e modal no per popover  tooltip
+// window.Popper = require('popper.js');
+// require('bootstrap');
+
+
+$(document).ready(function() {
+
+    // alert("ciao");
+
+});
